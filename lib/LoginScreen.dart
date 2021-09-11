9f09405f84972a7afea1cf589a5d7af6afd74b0f
SoumenduRoy <90237235+SoumenduRoy@users.noreply.github.com>
2021-09-11 18:26:11 +0530

    splash screen timer set, login page scroll view

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -28,171 +28,175 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           width: _screenWidth,
           height: _screenHeight,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child:
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child:Image.asset(
-                        'assets/images/loginlogo.png',
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        "LOGIN",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          // fontWeight: FontWeight.bold,
+
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child:Image.asset(
+                          'assets/images/loginlogo.png',
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 120,
-              ),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(top: 20),
-                child: Text(
-                  "    E-MAIL ADDRESS",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                ),
-              ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                keyboardType: TextInputType.phone,
-                onEditingComplete: null,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.mail_outline),
-                  contentPadding: EdgeInsets.all(20.0),
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1, color: Colors.white),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(20.0),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            // fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(top: 20),
-                child: Text(
-                  "    PASSWORD",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
+                SizedBox(
+                  height: 120,
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                keyboardType: TextInputType.visiblePassword,
-                obscureText:  true,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(20.0),
-                  prefixIcon: Icon(Icons.lock_outline),
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1, color: Colors.white),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                width: double.infinity,
-                // margin: EdgeInsets.only(top: 1),
-                child: TextButton(
-                  onPressed: null,
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 20),
                   child: Text(
-                    "FORGOTTEN PASSWORD?",
+                    "    E-MAIL ADDRESS",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                  ),
+                ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  onEditingComplete: null,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.mail_outline),
+                    contentPadding: EdgeInsets.all(20.0),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.white),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "    PASSWORD",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 12,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              SizedBox(
-                height: 55,
-                width: 180,
-                child: ElevatedButton(
-                  onPressed: null,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.yellow[600]),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText:  true,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(20.0),
+                    prefixIcon: Icon(Icons.lock_outline),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.white),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 1,
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: TextButton(
-                      onPressed: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>RegistrationScreen()));
-                      },
-                      child: Text(
-                        'Create A New Account ->',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  width: double.infinity,
+                  // margin: EdgeInsets.only(top: 1),
+                  child: TextButton(
+                    onPressed: null,
+                    child: Text(
+                      "FORGOTTEN PASSWORD?",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
                       ),
                     ),
-              ),
-            ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                SizedBox(
+                  height: 55,
+                  width: 180,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.yellow[600]),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 1,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: TextButton(
+                        onPressed: (){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>RegistrationScreen()));
+                        },
+                        child: Text(
+                          'Create A New Account ->',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
