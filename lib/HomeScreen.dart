cfabe98eec6df9682bafe93f19301f20210d7be7
SoumenduRoy <90237235+SoumenduRoy@users.noreply.github.com>
2021-09-19 19:46:04 +0530

    Create appbar row

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:protibimbo/CustomNavigationDrawer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -31,118 +32,93 @@ class HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Requests  ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 11,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 10.0,
+                      backgroundColor: Colors.green,
+                      child: Text('8',style: TextStyle(color: Colors.white,fontSize: 12),),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: null,
+            ),
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      'Notifications  ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 11,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 10.0,
+                      backgroundColor: Colors.indigo,
+                      child: Text('6',style: TextStyle(color: Colors.white,fontSize: 12),),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: null,
+            ),
+            InkWell(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      'Messages  ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 11,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 10.0,
+                      backgroundColor: Colors.deepOrange,
+                      child: Text('8',style: TextStyle(color: Colors.white,fontSize: 12),),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: null,
+            ),
+            SizedBox(width: 5,)
+          ],
+        ),
         backgroundColor: Color(0xffF3F3F3),
         elevation: 0,
         centerTitle: false,
         titleSpacing: 0,
       ),
-      drawer: Drawer(
-        backgroundColor: Color(0xffFFFFFF),
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-                child:Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 44.0,
-                      backgroundColor: Colors.indigo,
-                      child: CircleAvatar(
-                        radius: 40.0,
-                        backgroundColor: const Color(0xFF778899),
-                        backgroundImage:
-                        NetworkImage("http://tineye.com/images/widgets/mona.jpg"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                   Container(
-                     child: Column(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                         Text('Sudeshna Paul',
-                         style: TextStyle(color: Colors.black,
-                             fontSize: 20,
-                             fontWeight: FontWeight.bold),
-                         ),
-                         SizedBox(
-                           height: 10,
-                         ),
-                         Text('Photograper',
-                           style: TextStyle(color: Colors.grey,
-                             fontSize: 18,
-                           ),
-                         ),
-                       ],
-                     ),
-                   ),
-                  ],
-                ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home_outlined,
-                  color: Colors.black
-              ),
-              title: Text('Home'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/explore_icon.png',
-              height: 20,
-              width: 25,
-                color: Colors.black,
-              ),
-              title: Text('Explore'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/myPost_icon.png',
-                height: 20,
-                width: 25,
-                color: Colors.black,
-              ),
-              title: Text('My Post'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/playList_icon.png',
-                height: 20,
-                width: 25,
-                color: Colors.black,
-              ),
-              title: Text('Play List'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/event_icon.png',
-                height: 20,
-                width: 25,
-                color: Colors.black,
-              ),
-              title: Text('Event'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/termsAndCondition_icon.png',
-                height: 20,
-                width: 25,
-                color: Colors.black,
-              ),
-              title: Text('Terms & Condition'),
-              onTap: null,
-            ),
-            ListTile(
-              leading: Image.asset('assets/images/privacyPolicy_icon.png',
-                height: 20,
-                width: 25,
-                color: Colors.black,
-              ),
-              title: Text('Privacy Policy'),
-              onTap: null,
-            ),
-          ],
-        ),
-      ),
+      drawer: CustomNavigationDrawer(),
+      
     );
     }
 }
