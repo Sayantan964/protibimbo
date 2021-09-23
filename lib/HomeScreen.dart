3c9b1bbae91ef9948ffc0d927e8629d254c104c8
SoumenduRoy <90237235+SoumenduRoy@users.noreply.github.com>
2021-09-23 16:17:54 +0530

    add two row under scroll view

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,3 +1,4 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:protibimbo/CustomNavigationDrawer.dart';
 
@@ -118,7 +119,93 @@ class HomeScreenState extends State<HomeScreen> {
         titleSpacing: 0,
       ),
       drawer: CustomNavigationDrawer(),
-      
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        width: _screenWidth,
+        height: _screenHeight,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  InkWell(
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Your Feed               ',
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 14,
+                            ),
+                          ),
+                          Image.asset('assets/images/your_feed_icon.png'),
+                        ],
+                      ),
+                    ),
+                    onTap: null,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  InkWell(
+                    child: Container(
+                        padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                        child: Image.asset('assets/images/friends_icon.png'),
+                    ),
+                    onTap: null,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  InkWell(
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                      child: Image.asset('assets/images/group_icon.png'),
+                    ),
+                    onTap: null,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  InkWell(
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(13, 13, 13, 13),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border:Border.all(color: Colors.grey,width: .5),color: Color(0xffFFFFFF),),
+                      child: Image.asset('assets/images/search_icon.png'),
+                    ),
+                    onTap: null,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32.0,
+                    backgroundColor: Color(0xffFF3F80),
+                    child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: const Color(0xFF778899),
+                      backgroundImage:
+                      AssetImage('assets/images/profile_image.png'),
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
     }
 }
