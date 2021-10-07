a9540c05e2b2a9f92274849ead7686d296b0d9f5
Sayantan Das <70008764+Sayantan964@users.noreply.github.com>
2021-10-07 12:57:14 +0530

    Create UpdateProfileScreen.dart

---
+++ lib/views/screens/UpdateProfileScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UpdateProfileScreen extends StatefulWidget {
+  const UpdateProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _UpdateProfileScreenState createState() => _UpdateProfileScreenState();
+}
+
+class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
